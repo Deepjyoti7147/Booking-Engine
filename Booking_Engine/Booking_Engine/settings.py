48381dcf4303bcb69be1e2a251e8a75ca8b89093
Deepjyoti Purkayastha <deepjyoti.college@gmail.com>
2024-05-25 16:56:27 +0530

    Allowed all hosts

--- Booking_Engine/Booking_Engine/settings.py
+++ Booking_Engine/Booking_Engine/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9#@jbo66)$e(k8nld5+*-5#*j!w6q&!vtb+#uqt@om5vn0xo4^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
