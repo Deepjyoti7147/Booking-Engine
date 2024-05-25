e14cad2c2215cf417b7dc5ac585f9c97bf2933ef
Deepjyoti Purkayastha <deepjyoti.college@gmail.com>
2024-05-25 16:16:21 +0530

    First Code Push

--- Booking_Engine/HotelReg/models.py
+++ Booking_Engine/HotelReg/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Hotel(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.TextField()
+    phone_number = models.CharField(max_length=15)
+    bin = models.CharField(max_length=100, unique=True)
+    owner_name = models.CharField(max_length=255)
+    owner_email = models.EmailField(null=True, blank=True)
+    def save(self, *args, **kwargs):
+        if not self.bin:
+            self.bin = self.generate_bin()
+        super().save(*args, **kwargs)
+
+    def generate_bin(self):
+        import uuid
+        return str(uuid.uuid4())
+
